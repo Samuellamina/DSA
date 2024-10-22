2a040ebcefcf0374088f5dffc141ba449bffe986
samuellamina <samuellamina2@gmail.com>
2024-10-22 04:51:33 +0100

    strings dsa

---
+++ src/dataStructures/Strings/Strings.java
@@ -0,0 +1,12 @@
+package dataStructures.Strings;
+
+public class Strings {
+    public static void main(String[] args) {
+        String word = "az";
+        word.toCharArray();
+        System.out.println(word.toCharArray());
+    }
+
+
+}
+
